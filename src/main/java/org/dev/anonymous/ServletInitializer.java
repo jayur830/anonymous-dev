0a3db90679b91168aa33674d5cb8fbeb97dd386a
jayur830 <jayur830@naver.com>
2021-11-21 01:12:59 +0900

    first commit

---
+++ src/main/java/org/dev/anonymous/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.dev.anonymous;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AnonymousApplication.class);
+    }
+
+}
---
+++ src/test/java/org/dev/anonymous/AnonymousApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev.anonymous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
